1c876185d1bd038bd7b596d52b91ab300c1d0c33
Abir Tarchouni <abir.6tarchouni@gmail.com>
2024-12-29 19:20:18 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      fontFamily:{ 
+        amen:["Roboto"],
+        
+      },
+      colors:{
+        'abir':'rgb(243, 58, 106)',
+      },
+      },
+   
+
+  },
+  plugins: [],
+}
+
