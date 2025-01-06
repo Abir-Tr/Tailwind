279b314964416e12199bffeebe2e46cacc411577
Abir Tarchouni <abir.6tarchouni@gmail.com>
2025-01-06 20:59:54 +0100

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ module.exports = {
         
       },
       colors:{
-        'abir':'rgb(243, 58, 106)',
+        'abir':'rgb(245, 245, 220)',
+        'nude':'rgb(133,109,85)',
+        'light brown':'rgb(211,182,131)',
+        'paleoyster':'rgb(221,177,125)',
       },
       },
    
